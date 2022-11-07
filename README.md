ef2e897c7ae628919a2ce03979f063de22e9337d
eteillet <elie.teillet@gmail.com>
2022-11-07 15:52:08 +0100

    refacto syracuse

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Conjecture-de-Syracuse
+## Conjecture de Syracuse (ou conjecture de Collatz)
+
+La conjecture de Syracuse est l'hypothèse mathématique selon laquelle la suite de Syracuse de n'importe quel entier strictement positif atteint 1. Ce programme permet de constater cette simple mais mystérieuse théorie.
+
+######  Il n'y a pas réellement d'objectif scientifique, il a surtout permis de prolonger et approfondir une discussion avec un enfant de 8 ans (très) curieux et de vulgariser cette théorie. Un graphique vaut parfois mieux que les mots d'un papa!
+
+---
+##### Le principe est simple, pour tout entier strictement positif:
+    - s'il est pair, on le divise par 2
+    - s'il est impair on le multiplie par 3 et on ajoute 1
+
+En répétant l'opération, nous obtenons la suite de Syracuse... qui se termine inéluctablement par une boucle infinie 4,2,1,4,2,1,4,2,1,...
+https://fr.wikipedia.org/wiki/Conjecture_de_Syracuse
+
+### Usage
+    - définir l'entier ou la liste d'entiers à étudier dans args.json
+    - python syracuse.py
---
+++ syracuse.py
@@ -0,0 +1,40 @@
+
+import json
+import matplotlib.pyplot as plt
+
+def plot(n_lst: list):
+    for lst in n_lst:
+        plt.plot(lst, label=f"{lst[0]}: {len(lst)} iterations")
+    plt.xlim(left=0)
+    plt.ylim(bottom=0)
+    plt.xlabel("nb of iterations")
+    plt.ylabel("value")
+    plt.legend()
+    plt.title("Syracuse conjecture")
+    plt.show()
+
+def syracuse_algo(n: int):
+    values = []
+    while n != 1:
+        values.append(int(n))
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = n * 3 + 1
+    return values
+
+if __name__ == "__main__":
+    try:
+        param = json.load(open("args.json", "rb"))
+        numbers = param["numbers"]
+        if not all(x > 2 for x in numbers):
+            raise ValueError("Error: value < 2")
+        if len(numbers) > 10:
+            raise ValueError("Error: max number of values = 10")
+        numbers.sort()
+        result = []
+        for n in numbers:
+            result.append(syracuse_algo(n))
+        plot(result)
+    except Exception as e:
+        print(e)
